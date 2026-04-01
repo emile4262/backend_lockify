987bb5d45c334df13edae6934fed0d130f48b0fd
emile 4262 <bnandoemile@gmail.com>
2026-04-01 10:57:44 +0000

    fix app.controller et main

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,54 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { Response } from 'express';
 
 async function bootstrap() {
   console.log('--- ÉTAPE 1: Initialisation Nest ---');
+  
   try {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     console.log('--- ÉTAPE 2: AppModule chargé ---');
 
-    // ... tes configs (CORS, Swagger, etc.)
+    //Préfixe Global : Toutes tes routes seront sous /api (ex: /api/users/All)
+    app.setGlobalPrefix('api');
 
-  const port = parseInt(String(process.env.PORT), 10) || 8080;
-  console.log(`--- ÉTAPE 3: Tentative écoute sur port ${port} ---`);
+    // Fichiers statiques (Uploads)
+    app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+      prefix: '/uploads/',
+    });
+
+    //Configuration CORS (pour autoriser ton frontend)
+    app.enableCors({
+      origin: '*',
+    });
+
+    // Configuration SWAGGER
+    const config = new DocumentBuilder()
+      .setTitle('Lockify API')
+      .setDescription('Documentation de l’API backend sur Railway')
+      .setVersion('1.0')
+      .addTag('api')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    
+    // Le premier argument 'api' est le chemin de la doc. 
+    // Avec le global prefix, Swagger sera sur : /api
+    SwaggerModule.setup('api', app, document);
+
+    //Gestion du Port pour Railway
+    const port = process.env.PORT || 8080;
+    console.log(`--- ÉTAPE 3: Tentative écoute sur port ${port} ---`);
     
     await app.listen(port, '0.0.0.0');
-    console.log(`🚀 App running on port ${port}`);
+    
+    console.log(`🚀 Serveur prêt !`);
+    console.log(`📜 Swagger disponible sur : /api`);
+    
   } catch (error) {
-    console.error('❌ CRASH AU BOOTSTRAP:', error);
-    process.exit(1); // Force la sortie pour voir l'erreur dans Railway
+    console.error('❌ ERREUR CRITIQUE AU DÉMARRAGE :', error);
+    process.exit(1);
   }
 }
 
