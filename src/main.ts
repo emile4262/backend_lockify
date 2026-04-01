3cb3b78e45d8dbffc2db44fef81366dcb08032f9
emile 4262 <bnandoemile@gmail.com>
2026-04-01 09:35:28 +0000

    fix app.controller et main

--- src/main.ts
+++ src/main.ts
@@ -31,11 +31,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-    // Route test (IMPORTANT pour Railway)
-    app.getHttpAdapter().get('/', (req, res: Response) => {
-  res.send('API RUNNING 🚀');
-  }); 
-
     const port = process.env.PORT || 8080;
 
     await app.listen(port, '0.0.0.0');
