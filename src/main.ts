130f4bed31806a132dc7bce2b8ed8d7b9b262c9c
emile 4262 <bnandoemile@gmail.com>
2026-04-02 10:58:26 +0000

    main

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,46 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { join } from 'path';
-import { NestExpressApplication } from '@nestjs/platform-express';
-
+import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule } from "@nestjs/swagger";
+import { JwtAuthGuard } from "./guards/jwt-auth.guard";
+import { AppModule } from "./app.module";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder } from "@nestjs/swagger";
 async function bootstrap() {
-  console.log('--- ÉTAPE 1: Initialisation Nest ---');
-  
-  try {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    console.log('--- ÉTAPE 2: AppModule chargé ---');
+  const config = new DocumentBuilder()
+    .setTitle('Joseph Treso API')
+    .setDescription('API documentation for Joseph Treso')
+    .setVersion('1.0')
+    .addTag('api')
+    .addBearerAuth
+    // {
+    //   type: 'http',
+    //   scheme: 'bearer',
+    //   bearerFormat: 'JWT',
+    //   description: 'Entrez votre token JWT',
+    // },
+    // 'JWT-auth', // Nom qui correspond à ApiBearerAuth
+    ()
+    .build();
 
-    //Préfixe Global : Toutes tes routes seront sous /api (ex: /api/users/All)
-    app.setGlobalPrefix('api');
+  const app = await NestFactory.create(AppModule);
+  // Appel du bootstrap d'import
+  const importService = app.get('ImportService');
+  await importService.importFromExcel();
 
-    // Fichiers statiques (Uploads)
-    app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-      prefix: '/uploads/',
-    });
+  app.useGlobalGuards(app.get(JwtAuthGuard));
 
-    //Configuration CORS (pour autoriser ton frontend)
-    app.enableCors({
-      origin: '*',
-    });
-
-    // Configuration SWAGGER
-    const config = new DocumentBuilder()
-      .setTitle('api')
-      .setDescription('Documentation de l’API backend sur Railway')
-      .setVersion('1.0')
-      .addTag('api')
-      .addBearerAuth()
-      .build();
-
-    const document = SwaggerModule.createDocument(app, config);
-    
-    // Le premier argument 'api' est le chemin de la doc. 
-    SwaggerModule.setup('api', app, document);
-
-    //Gestion du Port pour Railway
-    const port = process.env.PORT || 8080;    
-    await app.listen(port, '0.0.0.0');
-    
-  } catch (error) {
-    process.exit(1);
-  }
+  app.enableCors({
+    origin: '*', // Change to your frontend URL in production
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('document', app, document);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+    }),
+  );
 }
-bootstrap();
+void bootstrap();
